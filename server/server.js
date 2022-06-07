153d4f5864580cbc2ceaa31f9856035c327ac3c1
Yasmin <yasmin.bunnie@gmail.com>
2022-06-07 21:53:13 +0100

    adding base typeDefs and Schemas and Apollo Server is Running

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+// Import typeDefs and Resolvers from Schema Folder
+const { typeDefs, resolvers } = require("./Schemas");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
