6dacfd49481dc8096873e863f87bca85fb661e3f
empipio <94937507+empipio@users.noreply.github.com>
2022-06-08 20:46:52 +0100

    auth/jwt functionality added, dependencies installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,30 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,10 +1,9 @@
 import { gql } from "@apollo/client";
 
-// will need token in this mutation for jwt but not yet set up auth
-
 export const SIGNUP = gql`
   mutation signup($username: String!, $password: String!) {
     addUser(username: $username, password: $password) {
+      token
       user {
         _id
         username
