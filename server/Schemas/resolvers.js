2a5874faf9cdde24c7f62a5621f92cdb7ea6338a
Yasmin <yasmin.bunnie@gmail.com>
2022-06-10 00:02:16 +0100

    created Post model and createPost mutations and resolvers

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Post = require("../models/Post");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
@@ -22,11 +23,34 @@ const resolvers = {
         return null;
       }
     },
+
+    // User Login Logic
     loginUser: async (parent, { username, password }) => {
       const user = await User.findOne({ username });
       const correctPw = await user.isCorrectPassword(password);
       const token = signToken(user);
       return { token, user };
+    },
+
+    // Add a post to database
+    createPost: async (
+      parent,
+      { image, location, website, distance, description }
+    ) => {
+      try {
+        const post = await Post.create({
+          image,
+          location,
+          website,
+          distance,
+          description,
+        });
+        return post;
+      } catch (error) {
+        console.log(error);
+        console.log("post error at createPost resolvers");
+        return null;
+      }
     },
   },
 };
