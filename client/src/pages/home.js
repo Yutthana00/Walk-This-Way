1c5249d267fdf0c410791720c836760e9e6dbf25
empipio <94937507+empipio@users.noreply.github.com>
2022-06-11 09:44:07 +0100

    auth moved from app to home

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,18 +1,24 @@
 import React from "react";
 import auth from "../utils/auth";
 
-// import SignupForm from "./signup";
-// import LoginForm from "./login";
+import SignupForm from "./signup";
+import LoginForm from "./login";
 
 import PostForm from "../components/createPost";
 
 const Home = () => {
-  return (
+  return auth.loggedIn ? (
     <div>
       <h1>Welcome</h1>
 
       <PostForm />
       <button onClick={auth.logout}>logout</button>
+    </div>
+  ) : (
+    <div>
+      <LoginForm />
+      <p>Don't have an account yet? Sign up below!</p>
+      <SignupForm />
     </div>
   );
 };
