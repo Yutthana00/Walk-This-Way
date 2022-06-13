0bd1e60b71bfbb4d4dea8c5470790a6415c7b10f
empipio <94937507+empipio@users.noreply.github.com>
2022-06-13 20:56:20 +0100

    commented out imageuploading for now

--- client/src/utils/imageUpload.js
+++ client/src/utils/imageUpload.js
@@ -1,29 +1,29 @@
 //import { Cloudinary } from "cloudinary-core";
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const ImageUpload = () => {
-  const [fileData, setFileData] = useState("");
-  const [images, setFile] = useState("");
+// const ImageUpload = () => {
+//   const [fileData, setFileData] = useState("");
+//   const [images, setFile] = useState("");
 
-  const handleFileChange = ({ target }) => {
-    setFileData(target.files[0]);
-    setFile(target.value);
+//   const handleFileChange = ({ target }) => {
+//     setFileData(target.files[0]);
+//     setFile(target.value);
 
-    return (
-      <form>
-        <CustomInput
-          type="file"
-          name="file"
-          accept="image/*"
-          onChange={handleFileChange}
-          placeholder="upload image"
-          isRequired={true}
-        />
-      </form>
-    );
-  };
-};
+//     return (
+//       <form>
+//         <CustomInput
+//           type="file"
+//           name="file"
+//           accept="image/*"
+//           onChange={handleFileChange}
+//           placeholder="upload image"
+//           isRequired={true}
+//         />
+//       </form>
+//     );
+//   };
+// };
 
-//var cl = new cloudinary.Cloudinary({ cloud_name: "drrjnrjac", secure: true });
+// //var cl = new cloudinary.Cloudinary({ cloud_name: "drrjnrjac", secure: true });
 
-export default ImageUpload;
+// export default ImageUpload;
