5680bcd8248e54f8456c6ab03023375140f70130
Yasmin <yasmin.bunnie@gmail.com>
2022-06-16 23:35:33 +0100

    reverse posts and added card component

--- client/src/components/getPosts.js
+++ client/src/components/getPosts.js
@@ -1,7 +1,7 @@
-import React from "react";
 import { useQuery } from "@apollo/client";
 
 import { GET_POSTS } from "../utils/queries";
+import Card from "./card";
 
 const GetPosts = () => {
   const { loading, data } = useQuery(GET_POSTS);
@@ -9,27 +9,19 @@ const GetPosts = () => {
   if (!posts.length) {
     return <h3>No Posts Yet</h3>;
   }
+
+  const reversedPosts = [...posts];
+  reversedPosts.reverse();
+
   return (
     <div>
       {loading ? (
         <div>Loading...</div>
       ) : (
         <div>
-          {posts?.length &&
-            posts.map((post) => (
-              <div key={post._id}>
-                <div>Location: {post.location}</div>
-                <div>Author: {post.author}</div>
-                <img
-                  src={post.image}
-                  style={{ height: 150, width: 150 }}
-                  alt={`posted by ${post.author}`}
-                />
-                <div>Distance: {post.distance}</div>
-                <div>Description: {post.description}</div>
-                <div>URL: {post.website}</div>
-              </div>
-            ))}
+          {reversedPosts.map((post) => (
+            <Card post={post} key={post._id} />
+          ))}
         </div>
       )}
     </div>
