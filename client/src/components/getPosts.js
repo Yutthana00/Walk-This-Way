6075d6831ae4ae1120f74af45150a1485d6e17d9
empipio <94937507+empipio@users.noreply.github.com>
2022-06-16 22:51:59 +0100

    added pollInterval to getPosts

--- client/src/components/getPosts.js
+++ client/src/components/getPosts.js
@@ -4,7 +4,7 @@ import { useQuery } from "@apollo/client";
 import { GET_POSTS } from "../utils/queries";
 
 const GetPosts = () => {
-  const { loading, data } = useQuery(GET_POSTS);
+  const { loading, data } = useQuery(GET_POSTS, { pollInterval: 3000 });
   const posts = data?.posts || [];
   if (!posts.length) {
     return <h3>No Posts Yet</h3>;
@@ -16,20 +16,22 @@ const GetPosts = () => {
       ) : (
         <div>
           {posts?.length &&
-            posts.map((post) => (
-              <div key={post._id}>
-                <div>Location: {post.location}</div>
-                <div>Author: {post.author}</div>
-                <img
-                  src={post.image}
-                  style={{ height: 150, width: 150 }}
-                  alt={`posted by ${post.author}`}
-                />
-                <div>Distance: {post.distance}</div>
-                <div>Description: {post.description}</div>
-                <div>URL: {post.website}</div>
-              </div>
-            ))}
+            posts
+              .map((post) => (
+                <div key={post._id}>
+                  <div>Location: {post.location}</div>
+                  <div>Author: {post.author}</div>
+                  <img
+                    src={post.image}
+                    style={{ height: 150, width: 150 }}
+                    alt={`posted by ${post.author}`}
+                  />
+                  <div>Distance: {post.distance}</div>
+                  <div>Description: {post.description}</div>
+                  <div>URL: {post.website}</div>
+                </div>
+              ))
+              .reverse()}
         </div>
       )}
     </div>
