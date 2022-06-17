d126090729e4ed327e924b1e1796c11fd7c07f64
Yasmin <yasmin.bunnie@gmail.com>
2022-06-17 23:53:44 +0100

    fixed navigation redirect

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -24,13 +24,14 @@ import {
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 import { useAuthContext } from "../utils/AuthProvider";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useUserContext } from "../utils/UserProvider";
 
 const Header = () => {
   const auth = useAuthContext();
   const { user, loggedIn } = useUserContext();
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     if (!auth.loggedIn()) {
@@ -39,10 +40,11 @@ const Header = () => {
   }, []);
 
   useEffect(() => {
-    if (!loggedIn) {
+    const ignoredPaths = ["/login", "/signup"];
+    if (!loggedIn && !ignoredPaths.includes(location?.pathname)) {
       navigate("/login");
     }
-  }, [loggedIn, navigate]);
+  }, [location?.pathname, loggedIn, navigate]);
 
   const { colorMode, toggleColorMode } = useColorMode();
   return (
@@ -129,234 +131,3 @@ const Header = () => {
 };
 
 export default Header;
-
-//   return (
-//     <>
-//       <Navbar sticky="top" bg="dark" expand="lg" variant="dark">
-//         <Container fluid>
-//           <NavLink to="/">
-//             <img
-//               src={require("./image/logo.png")}
-//               width="60"
-//               height="30"
-//               alt="Company logo"
-//             />{" "}
-//           </NavLink>
-
-//           <Navbar.Toggle aria-controls="navbarScroll" />
-//           <Navbar.Collapse id="navbarScroll">
-//             <Nav
-//               className="me-auto my-2 my-lg-0"
-//               style={{ maxHeight: "100px" }}
-//               navbarScroll
-//             >
-//               <Nav.Link href="/">Home</Nav.Link>
-//               <Nav.Link href="/signup">Sign Up</Nav.Link>
-//               <Nav.Link href="/login">Login</Nav.Link>
-//               <Nav.Link href="/FAQ">FAQ</Nav.Link>
-//               <Nav.Link href="/card">Card</Nav.Link>
-//               <Nav.Link href="/getPosts">Posts</Nav.Link>
-//               <Nav.Link href="/404">404</Nav.Link>
-//             </Nav>
-
-//             <Nav>
-//               <Button
-//                 variant="linear(to-r, #008900, #70cb00)"
-//                 onClick={auth.logout}
-//               >
-//                 Logout
-//               </Button>
-//             </Nav>
-//           </Navbar.Collapse>
-//         </Container>
-//       </Navbar>
-//     </>
-//   );
-// };
-
-// <NavDropdown title="Navdropdown" id="navbarScrollingDropdown">
-//   <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-//   <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-//   <NavDropdown.Divider />
-//   <NavDropdown.Item href="#action5">
-//     Something else here
-//   </NavDropdown.Item>
-// </NavDropdown>
-
-// export default Header;
-// import * as React from 'react';
-// import AppBar from '@mui/material/AppBar';
-// import Box from '@mui/material/Box';
-// import Toolbar from '@mui/material/Toolbar';
-// import IconButton from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
-// import Menu from '@mui/material/Menu';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import Container from '@mui/material/Container';
-// import Avatar from '@mui/material/Avatar';
-// import Button from '@mui/material/Button';
-// import Tooltip from '@mui/material/Tooltip';
-// import MenuItem from '@mui/material/MenuItem';
-// import AdbIcon from '@mui/icons-material/Adb';
-
-// const pages = ['Home', 'Post', 'FQA', 'Login', 'Logout'];
-// const settings = ['Home', 'Post', 'FQA', 'Login', 'Logout'];
-
-// // Responsivness
-// const Header = () => {
-//   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-//     null
-//   );
-//   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-//     null
-//   );
-
-//   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-//     setAnchorElNav(event.currentTarget);
-//   };
-//   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-//     setAnchorElUser(event.currentTarget);
-//   };
-
-//   const handleCloseNavMenu = () => {
-//     setAnchorElNav(null);
-//   };
-
-//   const handleCloseUserMenu = () => {
-//     setAnchorElUser(null);
-//   };
-
-//   return (
-//     <AppBar position="static" sx={{ background: '#000000' }}>
-//       <Container maxWidth="xl">
-//         <Toolbar disableGutters>
-//           {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
-//           {/* logo text */}
-
-//           <Typography
-//             variant="h6"
-//             noWrap
-//             component="a"
-//             href="/"
-//             sx={{
-//               mr: 2,
-//               display: { xs: 'none', md: 'flex' },
-//               fontFamily: 'monospace',
-//               fontWeight: 700,
-//               letterSpacing: '.3rem',
-//               color: 'inherit',
-//               textDecoration: 'none',
-//             }}
-//           >
-//             LOGO
-//           </Typography>
-
-//           {/* Hambermenu */}
-//           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-//             <IconButton
-//               size="large"
-//               aria-label="account of current user"
-//               aria-controls="menu-appbar"
-//               aria-haspopup="true"
-//               onClick={handleOpenNavMenu}
-//               color="inherit"
-//             >
-//               <MenuIcon />
-//             </IconButton>
-//             <Menu
-//               id="menu-appbar"
-//               anchorEl={anchorElNav}
-//               anchorOrigin={{
-//                 vertical: 'bottom',
-//                 horizontal: 'left',
-//               }}
-//               keepMounted
-//               transformOrigin={{
-//                 vertical: 'top',
-//                 horizontal: 'left',
-//               }}
-//               open={Boolean(anchorElNav)}
-//               onClose={handleCloseNavMenu}
-//               sx={{
-//                 display: { xs: 'block', md: 'none' },
-//               }}
-//             >
-//               {pages.map((page) => (
-//                 <MenuItem key={page} onClick={handleCloseNavMenu}>
-//                   <Typography textAlign="center">{page}</Typography>
-//                 </MenuItem>
-//               ))}
-//             </Menu>
-//           </Box>
-
-//           {/* Logo IMG */}
-//           {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
-//           <Typography
-//             variant="h5"
-//             noWrap
-//             component="a"
-//             href=""
-//             sx={{
-//               mr: 2,
-//               display: { xs: 'flex', md: 'none' },
-//               flexGrow: 1,
-//               fontFamily: 'monospace',
-//               fontWeight: 700,
-//               letterSpacing: '.3rem',
-//               color: 'inherit',
-//               textDecoration: 'none',
-//             }}
-//           >LOGO</Typography>
-//           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-//             {pages.map((page) => (
-//               <Button
-//                 key={page}
-//                 onClick={handleCloseNavMenu}
-//                 sx={{ my: 2, color: 'white', display: 'block' }}
-//               >
-//                 {page}
-//               </Button>
-//             ))}
-//           </Box>
-//           <Box sx={{ flexGrow: 0 }}>
-//             <Tooltip title="Open settings">
-//               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-
-//           {/* Avatar box */}
-//                 <Avatar variant="soft" color="neutral" size="lg"
-//                 color="neutral"
-//                 sx={{ ml: 'auto', alignSelf: 'flex-start' }}>
-//                 E
-//                 {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
-//               </Avatar>
-
-//               </IconButton>
-//             </Tooltip>
-//             <Menu
-//               sx={{ mt: '45px' }}
-//               id="menu-appbar"
-//               anchorEl={anchorElUser}
-//               anchorOrigin={{
-//                 vertical: 'top',
-//                 horizontal: 'right',
-//               }}
-//               keepMounted
-//               transformOrigin={{
-//                 vertical: 'top',
-//                 horizontal: 'right',
-//               }}
-//               open={Boolean(anchorElUser)}
-//               onClose={handleCloseUserMenu}
-//             >
-//               {settings.map((setting) => (
-//                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
-//                   <Typography textAlign="center">{setting}</Typography>
-//                 </MenuItem>
-//               ))}
-//             </Menu>
-//           </Box>
-//         </Toolbar>
-//       </Container>
-//     </AppBar>
-//   );
-// };
