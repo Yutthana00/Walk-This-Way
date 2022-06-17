dce34b95da53761301b4cc35b3f7ad0a28f75cbb
Yutthana00 <arf101293@gmail.com>
2022-06-17 09:28:20 +0100

    Finished card styling

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -13,61 +13,75 @@ const Card = ({ post }) => {
   return (
     <Center py={6}>
       <Box
-        maxW={"445px"}
+        maxW={"500px"}
         w={"full"}
         // eslint-disable-next-line react-hooks/rules-of-hooks
-        bg={useColorModeValue("white", "gray.900")}
+        bg={useColorModeValue("gray.900")}
         boxShadow={"2xl"}
         rounded={"md"}
         p={6}
         overflow={"hidden"}
       >
-        <Box
-          h={"210px"}
-          bg={"gray.100"}
-          mt={-6}
-          mx={-6}
-          mb={6}
-          pos={"relative"}
-        >
-          <Image src={post.image} layout={"fill"} />
-        </Box>
-        <Stack>
-          <Text
-            color={"green.500"}
-            textTransform={"uppercase"}
-            fontWeight={800}
-            fontSize={"sm"}
-            letterSpacing={1.1}
-          >
-            Blog
-          </Text>
-          <Heading
-            // eslint-disable-next-line react-hooks/rules-of-hooks
-            color={useColorModeValue("gray.700", "white")}
-            fontSize={"2xl"}
-            fontFamily={"body"}
-          >
-            {post.location}
-          </Heading>
-
-          <Text>{post.distance}</Text>
-          <Text>{post.description}</Text>
-          <Text>{post.website}</Text>
-        </Stack>
-        <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
+      <Stack direction={"row"} spacing={4} align={"center"}>
           <Avatar
             src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
             alt={"Author"}
           />
           <Stack direction={"column"} spacing={0} fontSize={"sm"}>
-            <Text fontWeight={600}>{post.author}</Text>
-            <Text color={"gray.500"}>Feb 08, 2021 · 6min read</Text>
+            <Text color={"gray.300"} fontWeight={600}>{post.author}</Text>
+            <Text color={"gray.500"}>Feb 08, 2021 </Text>
           </Stack>
         </Stack>
+
+        <Box
+
+          bg={"gray.100"}
+          mt={6}
+          mx={-6}
+          mb={4}
+          pos={"relative"}
+        >
+          <Image src={post.image} layout={"fill"} />
+        </Box>
+
+        <Stack>
+          <Box color={"gray.300"} mb={1}>
+          <Text
+          padding={'5px'}
+          color={'green.500'} 
+          fontWeight={800}
+          fontSize={'md'}
+          letterSpacing={1.1}>
+            {post.location} · {post.distance}km
+          </Text>
+
+          <Text color={'gray.500'}h={"8"}> websirte: {post.website}</Text>
+          
+          <Text
+          color={"gray.300"}
+          fontSize={"15px"}>
+          {post.description}</Text>
+          </Box>
+          </Stack>
+        
       </Box>
     </Center>
   );
 };
 
 export default Card;
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+// color={useColorModeValue("gray.700", "white")}
+// fontSize={"2xl"}
+// fontFamily={"body"}
+
+// <Text
+// color={"gray.400"}
+// textTransform={"uppercase"}
+// fontWeight={800}
+// fontSize={"sm"}
+// letterSpacing={1.1}
+// >
+// Blog
+// </Text>
