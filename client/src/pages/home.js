91e547778f8f19c62a3d3ec0ac538d57c686467d
Yasmin <yasmin.bunnie@gmail.com>
2022-06-17 02:49:27 +0100

    post button css

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import { Button, ButtonProps, Flex } from "@chakra-ui/react";
 import GetPosts from "../components/getPosts";
 import { useAuthContext } from "../utils/AuthProvider";
 // import "../signUp.css";
@@ -28,7 +28,34 @@ const Home = () => {
       <br />
 
       <Link to={"/createPost"}>
-        <button>Want to Create a Post? Click Me</button>
+        <Flex
+          margin="5"
+          justifyContent="right"
+          alignItems="right"
+          position="sticky"
+        >
+          <Button
+            /* flex={1} */
+            px={4}
+            fontSize={"sm"}
+            rounded={"full"}
+            bgGradient="linear(to-r, #008900, #70cb00)"
+            color={"white"}
+            boxShadow={
+              "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+            }
+            _hover={{
+              bg: "yellow.500",
+              transform: "translateY(-2px)",
+              boxShadow: "lg",
+            }}
+            _focus={{
+              bg: "blue.500",
+            }}
+          >
+            Post
+          </Button>
+        </Flex>
       </Link>
 
       <br />
